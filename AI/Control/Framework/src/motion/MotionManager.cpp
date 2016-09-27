6f575c792be5c9aef5b01acff01fcdc604fd2a5f
fei <isaac25silva@yahoo.com.br>
2016-09-27 14:31:09 -0300

    Update framework, include log file to write failure because temperature or communication

--- AI/Control/Framework/src/motion/MotionManager.cpp
+++ AI/Control/Framework/src/motion/MotionManager.cpp
@@ -1,9 +1,16 @@
-/*
- *   MotionManager.cpp
- *
- *   Author: ROBOTIS
- *
- */
+/*--------------------------------------------------------------------
+
+******************************************************************************
+* @file MotionManager.cpp
+* @author ROBOTIS
+* @version V2.0.0 - ROBOFEI-HT
+* @created -
+* @Modified Isaac Jesus da Silva - University Center FEI - 27/09/2016  😛
+* @e-mail isaac25silva@yahoo.com.br
+* @brief MotionManager 😛
+****************************************************************************
+**************************************************************************** 
+---------------------------------------------------------------------------*/
 
 #include <stdio.h>
 #include <math.h>
@@ -13,6 +20,8 @@
 #include <unistd.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <fstream>
+#include <time.h>
 #include <blackboard.h>
 
 using namespace Robot;
@@ -215,66 +224,10 @@ void MotionManager::Process()
 		
 		m_IsRunning = true;
 
-    // calibrate gyro sensor
-//    if(m_CalibrationStatus == 0 || m_CalibrationStatus == -1)
-//    {
-//        static int fb_gyro_array[GYRO_WINDOW_SIZE] = {512,};
-//        static int rl_gyro_array[GYRO_WINDOW_SIZE] = {512,};
-//        static int buf_idx = 0;
 
-//        if(buf_idx < GYRO_WINDOW_SIZE)
-//        {
-//            if(m_CM730->m_BulkReadData[CM730::ID_CM].error == 0)
-//            {
-//                fb_gyro_array[buf_idx] = m_CM730->m_BulkReadData[CM730::ID_CM].ReadWord(CM730::P_GYRO_Y_L);
-//                rl_gyro_array[buf_idx] = m_CM730->m_BulkReadData[CM730::ID_CM].ReadWord(CM730::P_GYRO_X_L);
-//                buf_idx++;
-//            }
-//        }
-//        else
-//        {
-//            double fb_sum = 0.0, rl_sum = 0.0;
-//            double fb_sd = 0.0, rl_sd = 0.0;
-//            double fb_diff, rl_diff;
-//            double fb_mean = 0.0, rl_mean = 0.0;
 
-//            buf_idx = 0;
+        m_CalibrationStatus = 1;
 
-//            for(int i = 0; i < GYRO_WINDOW_SIZE; i++)
-//            {
-//                fb_sum += fb_gyro_array[i];
-//                rl_sum += rl_gyro_array[i];
-//            }
-//            fb_mean = fb_sum / GYRO_WINDOW_SIZE;
-//            rl_mean = rl_sum / GYRO_WINDOW_SIZE;
-
-//            fb_sum = 0.0; rl_sum = 0.0;
-//            for(int i = 0; i < GYRO_WINDOW_SIZE; i++)
-//            {
-//                fb_diff = fb_gyro_array[i] - fb_mean;
-//                rl_diff = rl_gyro_array[i] - rl_mean;
-//                fb_sum += fb_diff * fb_diff;
-//                rl_sum += rl_diff * rl_diff;
-//            }
-//            fb_sd = sqrt(fb_sum / GYRO_WINDOW_SIZE);
-//            rl_sd = sqrt(rl_sum / GYRO_WINDOW_SIZE);
-
-//            if(fb_sd < MARGIN_OF_SD && rl_sd < MARGIN_OF_SD)
-//            {
-//                m_FBGyroCenter = (int)fb_mean;
-//                m_RLGyroCenter = (int)rl_mean;
-                m_CalibrationStatus = 1;
-//                if(DEBUG_PRINT == true)
-//                    fprintf(stderr, "FBGyroCenter:%d , RLGyroCenter:%d \n", m_FBGyroCenter, m_RLGyroCenter);
-//            }
-//            else
-//            {
-//                m_FBGyroCenter = 512;
-//                m_RLGyroCenter = 512;
-//                m_CalibrationStatus = -1;
-//            }
-//        }
-//    }
 
     if(m_CalibrationStatus == 1 && m_Enabled == true)
     {
@@ -301,16 +254,6 @@ void MotionManager::Process()
           MotionStatus::FB_GYRO = (1.0 - GYRO_ALPHA) * MotionStatus::FB_GYRO + GYRO_ALPHA * gyroValFB;
           MotionStatus::RL_GYRO = (1.0 - GYRO_ALPHA) * MotionStatus::RL_GYRO + GYRO_ALPHA * gyroValRL;
 
-
-//					fb_array[buf_idx] = MotionStatus::FB_ACCEL;
-//          if(++buf_idx >= ACCEL_WINDOW_SIZE) buf_idx = 0;
-
-//           const double TICKS_TO_RADIANS_PER_STEP = (M_PI/180.0) * 250.0/512.0 * (0.001 * MotionModule::TIME_UNIT);
-//           m_angleEstimator.predict(
-//              -TICKS_TO_RADIANS_PER_STEP * gyroValFB,
-//              TICKS_TO_RADIANS_PER_STEP * gyroValRL,
-//              0
-//           );
 
 //           m_angleEstimator.update(
 //               (m_CM730->m_BulkReadData[CM730::ID_CM].ReadWord(CM730::P_ACCEL_X_L) - 512),
@@ -373,134 +316,10 @@ void MotionManager::Process()
 	if(joint_num > 0)
 	m_CM730->SyncWrite(MX28::P_CW_COMPLIANCE_SLOPE, MX28::PARAM_BYTES, joint_num, param);
 
-//int param[JointData::NUMBER_OF_JOINTS * 5];
-//int wGoalPosition, wStartPosition, wDistance;
-//int n = 0;
 
-
-//        if(m_Modules.size() != 0)
-//        {
-//            for(std::list<MotionModule*>::iterator i = m_Modules.begin(); i != m_Modules.end(); i++)
-//            {
-//                (*i)->Process();
-//                for(int id=JointData::ID_MIN; id<=JointData::ID_MAX-2; id++)
-//                {
-//                    if((*i)->m_Joint.GetEnable(id) == true)
-//                    {
-//                        MotionStatus::m_CurrentJoints.SetSlope(id, (*i)->m_Joint.GetCWSlope(id), (*i)->m_Joint.GetCCWSlope(id));
-//                        MotionStatus::m_CurrentJoints.SetValue(id, (*i)->m_Joint.GetValue(id));
-
-
-//				wStartPosition = MotionStatus::m_CurrentJoints.GetValue(id);
-//				wGoalPosition = (*i)->m_Joint.GetValue(id) + m_Offset[id];
-//					if( wStartPosition > wGoalPosition )
-//				 		wDistance = wStartPosition - wGoalPosition;
-//					 else
-// 							wDistance = wGoalPosition - wStartPosition;
-
-// 					wDistance >>= 2;
-// 					if( wDistance < 8 )
-// 						wDistance = 8;
-//					 int value;
-//					 m_CM730->ReadWord(id, MX28::P_GOAL_POSITION_L, &value, 0);
-//					 param[n++] = id;
-//                	 param[n++] = MotionStatus::m_CurrentJoints.GetCWSlope(id);
-//               		 param[n++] = MotionStatus::m_CurrentJoints.GetCCWSlope(id);
-//					 param[n++] = CM730::GetLowByte(wGoalPosition);
-//					 param[n++] = CM730::GetHighByte(wGoalPosition);
-//					 //param[n++] = CM730::GetLowByte(wDistance);
-//					 //param[n++] = CM730::GetHighByte(wDistance);
-
-//						//m_CM730->WriteWord(id, MX28::P_GOAL_POSITION_L, int((double((*i)->m_Joint.GetValue(id)))) +adaptValueGait[id-1], 0);
-
-//						//m_CM730->WriteWord(id, MX28::P_GOAL_POSITION_L, int((double((*i)->m_Joint.GetValue(id)))) +m_Offset[id], 0);
-
-//                        //MotionStatus::m_CurrentJoints.SetPGain(id, (*i)->m_Joint.GetPGain(id));
-////                        MotionStatus::m_CurrentJoints.SetIGain(id, (*i)->m_Joint.GetIGain(id));
-////                        MotionStatus::m_CurrentJoints.SetDGain(id, (*i)->m_Joint.GetDGain(id));
-//                    }
-//                }
-////				m_CM730->SyncWrite(MX28::P_GOAL_POSITION_L, 3, JointData::NUMBER_OF_JOINTS - 3, param);
-//              m_CM730->SyncWrite(MX28::P_CW_COMPLIANCE_SLOPE, 5, JointData::NUMBER_OF_JOINTS - 3, param);
-//            }
-//        }
-
- 		
-
-//        int param[(JointData::NUMBER_OF_JOINTS) * MX28::PARAM_BYTES];
-//        int n = 0;
-//        int joint_num = 0;
-//        for(int id=JointData::ID_MIN; id<=JointData::ID_MAX; id++) //tirando a cabeça
-//        {
-//            //if(MotionStatus::m_CurrentJoints.GetEnable(id) == true)
-//            {
-//                param[n++] = id;
-//////////#ifdef MX28_1024
-//                param[n++] = MotionStatus::m_CurrentJoints.GetCWSlope(id);
-//                param[n++] = MotionStatus::m_CurrentJoints.GetCCWSlope(id);
-//////////#else
-//////////                param[n++] = MotionStatus::m_CurrentJoints.GetDGain(id);
-//////////                param[n++] = MotionStatus::m_CurrentJoints.GetIGain(id);
-//////////                int p_gain = m_voltageAdaptionFactor * MotionStatus::m_CurrentJoints.GetPGain(id);
-//////////                if(p_gain <= 0)
-//////////                    p_gain = 1;
-//////////                param[n++] = p_gain;
-//////////                param[n++] = 0;
-//////////#endif
-//                param[n++] = MotionStatus::m_CurrentJoints.GetValue(id) + m_Offset[id];
-//                param[n++] = MotionStatus::m_CurrentJoints.GetValue(id) + m_Offset[id];
-//								joint_num++;
-//            }
-
-////            if(DEBUG_PRINT == true)
-////                fprintf(stderr, "ID[%d] : %d \n", id, MotionStatus::m_CurrentJoints.GetValue(id));
-//        }
-
-//        if(joint_num > 0)
-////#ifdef MX28_1024
-//            m_CM730->SyncWrite(MX28::P_CW_COMPLIANCE_SLOPE, MX28::PARAM_BYTES, joint_num, param);
-////#else
-////            m_CM730->SyncWrite(MX28::P_D_GAIN, MX28::PARAM_BYTES, joint_num, param);
-////#endif
-////			unsigned int ic=0;
-////    		while(ic < m_CM730->m_DelayedWords)
-////			{
-////			m_CM730->WriteWord(m_CM730->m_DelayedAddress[ic],m_CM730->m_DelayedWord[ic],0);
-////			ic++;
-////			}
-////			m_CM730->m_DelayedWords = 0;
 	}
 		
 
-
-//		m_CM730->BulkRead();
-//		// update joint temps
-//   	   for(int id=JointData::ID_MIN; id<=JointData::ID_MAX-2; id++)//tirando a cabeça
-//       {
-//       		if(MotionStatus::m_CurrentJoints.GetEnable(id) == true)
-//			{
-//					int value = m_CM730->m_BulkReadData[id].ReadByte(MX28::P_PRESENT_TEMPERATURE);
-//					MotionStatus::m_CurrentJoints.SetTemp(id,value);
-//			}
-//	   }
-//    if(m_IsLogging)
-//    {
-//        for(int id = JointData::ID_MIN; id <= JointData::ID_MAX-2; id++)//tirando a cabeça
-//            m_LogFileStream << MotionStatus::m_CurrentJoints.GetValue(id) << "," << m_CM730->m_BulkReadData[id].ReadWord(MX28::P_PRESENT_POSITION_L) << ",";
-
-//        m_LogFileStream << m_CM730->m_BulkReadData[CM730::ID_CM].ReadWord(CM730::P_GYRO_Y_L) << ",";
-//        m_LogFileStream << m_CM730->m_BulkReadData[CM730::ID_CM].ReadWord(CM730::P_GYRO_X_L) << ",";
-//        m_LogFileStream << m_CM730->m_BulkReadData[CM730::ID_CM].ReadWord(CM730::P_ACCEL_Y_L) << ",";
-//        m_LogFileStream << m_CM730->m_BulkReadData[CM730::ID_CM].ReadWord(CM730::P_ACCEL_X_L) << ",";
-//        m_LogFileStream << m_CM730->m_BulkReadData[FSR::ID_L_FSR].ReadByte(FSR::P_FSR_X) << ",";
-//        m_LogFileStream << m_CM730->m_BulkReadData[FSR::ID_L_FSR].ReadByte(FSR::P_FSR_Y) << ",";
-//        m_LogFileStream << m_CM730->m_BulkReadData[FSR::ID_R_FSR].ReadByte(FSR::P_FSR_X) << ",";
-//        m_LogFileStream << m_CM730->m_BulkReadData[FSR::ID_R_FSR].ReadByte(FSR::P_FSR_Y) << ",";
-//        m_LogFileStream << "\x0d\x0a";
-//    }
-
-//    if(m_CM730->m_BulkReadData[CM730::ID_CM].error == 0)
-//        MotionStatus::BUTTON = m_CM730->m_BulkReadData[CM730::ID_CM].ReadByte(CM730::P_BUTTON);
     m_IsRunning = false;
 
     if(m_torque_count != DEST_TORQUE && --m_torqueAdaptionCounter == 0)
@@ -555,6 +374,7 @@ void MotionManager::adaptTorqueToVoltage()
     	if(count_fail>=4)
     	{
     		printf("Falha na comunicação: Chave provavelmente desligada\n");
+    		logServo(); //Escreve no arquivo de log a hora que terminou o processo
     		exit(0);
     	}
        	return;
@@ -568,6 +388,7 @@ void MotionManager::adaptTorqueToVoltage()
         {
 		    printf("Tensão Abaixo do recomendado | Tensão = %2.1fV\n", (float)voltage/(float)10);
 		    printf("A bateria deve ser trocada\n");
+		    logVoltage(voltage); //Escreve no arquivo de log a tensão e a hora que terminou o processo
 		    exit(0);
 		}
     }
@@ -586,4 +407,38 @@ void MotionManager::adaptTorqueToVoltage()
 #endif
 
     m_CM730->WriteWord(CM730::ID_BROADCAST, MX28::P_TORQUE_LIMIT_L, torque, 0);
+}
+
+void MotionManager::logVoltage(int voltage)
+{
+        std::fstream File;
+        time_t _tm =time(NULL);
+        struct tm * curtime = localtime ( &_tm );
+        File.open("../../Control/Control.log", std::ios::app | std::ios::out);
+        if (File.good() && File.is_open())
+        {
+            File << "Tensão Abaixo do recomendado | Tensão = "<<(float)voltage/(float)10<<"V --- ";
+            File << asctime(curtime);
+            File.flush();
+            File.close();
+        }
+        else
+	        printf("Erro ao Salvar o arquivo\n");
+}
+
+void MotionManager::logServo()
+{
+        std::fstream File;
+        time_t _tm =time(NULL);
+        struct tm * curtime = localtime ( &_tm );
+        File.open("../../Control/Control.log", std::ios::app | std::ios::out);
+        if (File.good() && File.is_open())
+        {
+            File << "Falha na comunicação: Chave provavelmente desligada"<<" --- ";
+            File << asctime(curtime);
+            File.flush();
+            File.close();
+        }
+        else
+	        printf("Erro ao Salvar o arquivo\n");
 }
