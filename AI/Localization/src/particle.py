d5a687fd0b67d06aec3b36a4c69711de1f99dab1
Aislan Cesar <c.aislan@hotmail.com>
2016-11-23 17:41:31 -0200

    New Motion Model

--- AI/Localization/src/particle.py
+++ AI/Localization/src/particle.py
@@ -57,10 +57,11 @@ class Particle(object):
         self.weight = weight # Holds particles weight, can come from previous iterations
 
         # Motion error coefficients
-        # See Probabilistic Robotics, page 124, Table 5.3 for more details.
-        # a0: ordem(-2), a1: ordem(1)
-        # a2: ordem(-4), a3: ordem(-1)
-        # a4: ordem(-4), a4: ordem(-1)
+        # a0: ordem(), a1: ordem(), a2: ordem(), a3: ordem()
+        # a4: ordem(), a5: ordem(), a6: ordem(), a7: ordem()
+        # a8: ordem(), a9: ordem(), a10: ordem(), a11: ordem()
+        # a12: ordem(), a13: ordem(), a14: ordem(), a15: ordem()
+
         if a == None:
             a0 = rnd.gauss(0.007, 0.002)
             a1 = rnd.gauss(7, 2)
@@ -68,7 +69,7 @@ class Particle(object):
             a3 = rnd.gauss(0.07, 0.02)
             a4 = rnd.gauss(0.00007, 0.00002)
             a5 = rnd.gauss(0.07, 0.02)
-            self.a = (a0, a1, a2, a3, a4, a5)
+            self.a = (a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15)
         else:
             self.a = a
 
@@ -79,35 +80,44 @@ class Particle(object):
             self.std == std
 
     #----------------------------------------------------------------------------------------------
-    #   Method which moves particles around. (Probabilistic Robotics, pg 124, table 5.1)
+    #   Method which moves particles around, reimplement.
     #----------------------------------------------------------------------------------------------
-    def Motion(self, translational=0, rotational=0, dt=0):
-        # translational is the robot's speed in cm/s
+    def Motion(self, straight=0, drift=0, rotational=0, moving=1, dt=0):
+        # straight is the robot's forward speed in cm/s
+        # drift is the robot's sideways speed in cm/s
         # rotational is the robot's angular speed in degrees/s
+        # moving is a boolean which caracterizes a moving robot.
 
         rtt = radians(rotational) # converts rotational from degrees to radians
         
-        # line 2:
-        v = rnd.gauss(translational, self.a[0]*translational**2+self.a[1]*rtt**2)
-        # line 3:
-        w = rnd.gauss(rtt, self.a[2]*translational**2+self.a[3]*rtt**2)
-        # line 4:
-        g = rnd.gauss(0, self.a[4]*translational**2+self.a[5]*rtt**2)
-        
-        theta = radians(self.rotation) # converts particle's rotation to radians
+        # Adds a gaussian error to thhe forward speed
+        F = rnd.gauss(straight, self.a[0]*straight**2 + self.a[1]*drift**2 + self.a[2]*rotational + self.a[3]*moving)
+        # Adds a gaussian error to the drift speed
+        D = rnd.gauss(drift, self.a[4]*straight**2 + self.a[5]*drift**2 + self.a[6]*rotational + self.a[7]*moving)
+        # Adds a gaussian error to the rotational speed
+        W = rnd.gauss(rotational, self.a[8]*straight**2 + self.a[9]*drift**2 + self.a[10]*rotational + self.a[11]*moving)
+        # Adds an error to the final rotational position
+        g = rnd.gauss(0, self.a[12]*straight**2 + self.a[13]*drift**2 + self.a[14]*rotational + self.a[15]*moving)
+
+
+        theta = radians(self.rotation) # converts particle's rotation to radians.
+
         # In case of angle been smaller than 1 degree, execute this part
-        
-        if degrees(abs(w)) < 1:
-            self.x += v * cos(theta) * dt # delocation of x position
-            self.y += v * sin(theta) * dt # delocation of y position
+        if degrees(abs(W)) < 1:
+            self.y += (D*sin(theta) + F*cos(theta))*dt # X position motion
+            self.y += (D*cos(theta) + F*sin(theta))*dt # Y position motion
         else:
-            
-            # line 5:
-            self.x += -v/w*sin(theta) + v/w*sin(theta + w*dt) 
-            # line 6:
-            self.y += v/w*cos(theta) - v/w*cos(theta + w*dt)
-        # line 7:
-        self.rotation = degrees(theta + w*dt + g*dt)
+            # Auxiliar pre-computation
+            st = sin(theta)
+            sw = sin(W*dt)
+            ct = cos(theta)
+            cw = cos(W*dt)
+
+            self.x += (D*st*sw - D*ct*cw + F*st*cw + F*ct*sw)/W # X position motion
+            self.y += (D*st*cw + D*ct*sw + F*st*sw - F*ct*cw)/W # Y position motion
+        
+        # Final particle rotation
+        self.rotation = degrees(theta + W*dt + g*dt)
 
     #----------------------------------------------------------------------------------------------
     #   Likelihood computation
