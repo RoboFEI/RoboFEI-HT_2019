64a0d1e9b0a23e17fafebbbb06ee7336b8f24a2c
Vinicius <vinicius.nicassio@gmail.com>
2017-05-08 16:28:55 -0300

    Script de Commit da Visão

---
+++ AI/Vision/CommitVision.sh
@@ -0,0 +1,32 @@
+blue='\33[0;34m'
+NC='\33[0m' # No Color
+#font colors:
+#Black				0;30		Dark Gray			1;30
+#Blue					0;34		Light Blue		1;34
+#Green				0;32		Light Green		1;32
+#Cyan					0;36		Light Cyan		1;36
+#Red					0;31		Light Red			1;31
+#Purple				0;35		Light Purple	1;35
+#Brown/Orange	0;33		Yellow				1;33
+#Light Gray		0;37		White					1;37
+
+
+git config --global user.name ''
+git config --global user.email ''
+
+git config user.name ''
+git config user.email ''
+
+echo "${blue}Insira seu nome: ${NC}"
+read name
+
+echo "${blue}Insira seu email do git: ${NC}"
+read email
+
+git config user.name ${name}
+git config user.email ${email}
+
+git cola
+
+git config user.name ''
+git config user.email ''
