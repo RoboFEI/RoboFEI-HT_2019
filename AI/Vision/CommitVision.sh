0dbb1cf97759e99ebd35189801a4221fef73f03d
Marcelo <marcelo.macedo330@hotmail.com>
2017-05-09 17:37:11 -0300

    Atualizando o commit
    atualizar o commit

---
+++ AI/Vision/CommitVision.sh
@@ -0,0 +1,31 @@
+blue='\33[0;34m'
+NC='\33[0m' # No Color
+#font colors:
+#Black				0;30		Dark Gray			1;30
+#Blue					0;34		Light Blue		1;34
+#Green				0;32		Light Green		1;32
+#Cyan					0;36		Light Cyan		1;36
+#Red					0;31		Light Red			1;31
+#Purple				0;35		Light Purple	1;35
+#Brown/Orange	0;33		Yellow				1;33
+#Light Gray		0;37		White					1;37
+
+git config --global --unset-all user.name
+git config --global --unset-all user.email
+
+git config --unset-all user.name
+git config --unset-all user.email
+
+echo "${blue}Insira seu nome: ${NC}"
+read name
+
+echo "${blue}Insira seu email do git: ${NC}"
+read email
+
+git config --global user.name "${name}"
+git config --global user.email "${email}"
+
+git cola
+
+git config --global --unset-all user.name
+git config --global --unset-all user.email
