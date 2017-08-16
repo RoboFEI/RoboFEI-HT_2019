88207939829fec0cc6439ae2bedbbe8d850d90db
Vinicius Nicassio Ferreira <vinicius.nicassio@gmail.com>
2017-08-16 19:19:46 -0300

    Comentando MainFunctions

--- AI/Visual_Memory/src/MainFunctions.py
+++ AI/Visual_Memory/src/MainFunctions.py
@@ -7,14 +7,17 @@ import signal # Class used for external interrupt detection.
 from VisualMemoryException import *  # Used to handle exceptions
 
 ## signal_term_handler
+# Function performed in external kill.
 def signal_term_handler(signal, frame):
 	raise VisionException(3, '')
 
 ## killedProcess
+# Configure the function to be used when an external closure is requested.
 def killedProcess( ):
 	signal.signal(signal.SIGTERM, signal_term_handler)
 
 ## readDataLandmarks
+# Function created for reading landmarks data.
 def readDataLandmarks(taglandmark, mem):
 	# Reading landmarks value
 	x = mem.read_float('VISION_LAND_X')
