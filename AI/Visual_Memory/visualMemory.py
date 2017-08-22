88b11c8c7241afe2a7f3425eb478738067c9fbe3
Vinicius Nicassio Ferreira <vinicius.nicassio@gmail.com>
2017-08-22 10:52:15 -0300

    Atualizando estrutura

--- AI/Visual_Memory/visualMemory.py
+++ AI/Visual_Memory/visualMemory.py
@@ -58,7 +58,8 @@ killedProcess() # Recognize external kill
 # Default values
 parameters = {
 	"robot_numbers": 8,
-	"frequency_of_execution": 0,
+	"relation_measurement_and_robot": 80,
+	"frequency_of_execution": 0.2,
 }
 
 # Reading "config.ini" values
@@ -83,10 +84,11 @@ land = Landmark(speeds) # Creating landmark object
 ball = Ball(speeds) # Creating landmark object
 
 # Creating robot objects
+observation = []
 robots = []
 newrobots = []
 for __ in xrange(parameters["robot_numbers"]):
-	newrobots.append(Robots(speeds))
+	newrobots.append(Robots(speeds, observation))
 
 # Main loop
 
@@ -106,10 +108,93 @@ while True:
 		else:
 			land.prediction( )
 		
-		datarobots = readDataRobots(mem, parameters["robot_numbers"])
-		
-	#	 if datarobots == []:
+	#	 datarobots = readDataRobots(mem, parameters["robot_numbers"])
+		datarobots = [
+			[1, 5, 0, 0],
+			[1, 5, 0, 1],
+			[2, 5, 0, 2],
+			[2, 5, 0, 3],
+			[2, 5, 0, 4],
+			[1, 5, 0, 5],
+			[2, 5, 0, 6],
+			[1, 5, 0, 7],
+			[2, 5, 0, 8],
+			[2, 5, 0, 9],
+			[2, 5, 0, 10],
 			
+			[1, 0, 0, 0],
+			[1, 1, 1, 1],
+			[1, 2, 2, 2],
+			[2, 3, 3, 3],
+			[1, 4, 4, 4],
+			[2, 5, 5, 5],
+			[1, 6, 6, 6],
+			[1, 7, 7, 7],
+			[1, 8, 8, 8],
+			[2, 9, 9, 9],
+			[2, 10, 10, 10],
+			
+			[1, 10, 10, 0],
+			[1, 9, 9, 1],
+			[1, 8, 8, 2],
+			[1, 7, 7, 3],
+			[3, 6, 6, 4],
+			[3, 5, 5, 5],
+			[1, 4, 4, 6],
+			[3, 3, 3, 7],
+			[3, 2, 2, 8],
+			[3, 1, 1, 9],
+			[1, 0, 0, 10],
+			
+			[1, 5, 5, 0],
+			[3, 7, 6, 1],
+			[3, 9, 7, 2],
+			[1, 11, 8, 3],
+			[1, 13, 9, 4],
+			[3, 15, 10, 5],
+			[3, 15, 10, 6],
+			[1, 15, 10, 7],
+			[3, 15, 10, 8],
+			[3, 15, 10, 9],
+			[3, 15, 10, 10],
+		]
+		
+		if datarobots != []:
+			oldtime = datarobots[0][3]
+			datarobots = copy(robots)
+			
+			obs = 0
+			while(obs < len(datarobots)):
+				if oldtime != obs[3]:
+					datarobots = copy(robots)
+					oldtime = datarobots[obs][3]
+				
+				if datarobots is []:
+					newrobots[0].insertObservation(datarobots[obs])
+					robots.append(newrobots.pop(0))
+					datarobots.remove(datarobots[obs])
+					obs -= 1
+					continue
+				
+				observation = datarobots[obs]
+				
+				sort(robots, reverse = True)
+				
+				if datarobots[0].weight < float(parameters['relation_measurement_and_robot'])/100:
+					if newrobots != []:
+						newrobots[0].insertObservation(datarobots[obs])
+						robots.append(newrobots.pop(0))
+						datarobots.remove(datarobots[obs])
+						obs -= 1
+						continue
+				else:
+					datarobots[0].insertObservation(datarobots[obs])
+					datarobots.pop(0)
+					datarobots.remove(datarobots[obs])
+					obs -= 1
+				
+				obs += 1
+						
 		# stop timestamp
 		diff = time.time() - start
 		
