f13d03747a10401502a98e7490e92c53e2e3564f
SylvioRubens <srjneto@hotmail.com>
2017-08-31 20:46:52 -0300

    testes

--- AI/Odometry/Odometry.cpp
+++ AI/Odometry/Odometry.cpp
@@ -11,9 +11,9 @@ int main()
 	ini = new minIni((char *)INI_FILE_PATH);
 	int *mem = using_shared_memory(ini->getd("Communication","no_player_robofei",-1024) * 100); 
 
-	int x = read_int(mem, IMU_EULER_X);
-	int y = read_int(mem, IMU_EULER_Y);
-	int z = read_int(mem, IMU_EULER_Z);
+	float x = read_float(mem, IMU_EULER_X);
+	float y = read_float(mem, IMU_EULER_Y);
+	float z = read_float(mem, IMU_EULER_Z);
 	
 	cout<<"Variacao em torno do eixo x: "<<x<<endl;
 	cout<<"Variacao em torno do eixo y: "<<y<<endl;
