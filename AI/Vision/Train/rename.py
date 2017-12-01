aab9b9a8185763193d7efff1a4ee67dfdf0c7daa
Vinicius Nicassio Ferreira <vinicius.nicassio@gmail.com>
2017-12-01 15:40:56 -0200

    Atualizando Treinamentos

--- AI/Vision/Train/rename.py
+++ AI/Vision/Train/rename.py
@@ -18,13 +18,13 @@ while size > 0:
 
 for name in names:
 #     print name
-    sub = "sed -i 's/"+ name.replace("\ ", " ") +"/a"+ str(image).zfill(5) +"/g' ./Train/annotations/"+ name +".xml"
+    sub = "sed -i 's/"+ name.replace("\ ", " ") +"/a"+ str(image).zfill(digits) +"/g' ./Train/annotations/"+ name +".xml"
 #     print sub
     os.system(sub)
-    jpg = "mv ./Train/imagensTrain/"+ name +".jpg ./Train/imagensTrain/a"+ str(image).zfill(5) +".jpg"
+    jpg = "mv ./Train/imagensTrain/"+ name +".jpg ./Train/imagensTrain/a"+ str(image).zfill(digits) +".jpg"
 #     print jpg
     os.system(jpg)
-    xml = "mv ./Train/annotations/"+ name +".xml ./Train/annotations/a"+ str(image).zfill(5) +".xml"
+    xml = "mv ./Train/annotations/"+ name +".xml ./Train/annotations/a"+ str(image).zfill(digits) +".xml"
 #     print xml
     os.system(xml)
 #     raw_input()
@@ -40,10 +40,10 @@ while size > 0:
     digits += 1
 
 for name in names:
-    sub = "sed -i 's/"+ name.replace("\ ", " ") +"/image"+ str(image).zfill(5) +"/g' ./Train/annotations/"+ name +".xml"
+    sub = "sed -i 's/"+ name.replace("\ ", " ") +"/image"+ str(image).zfill(digits) +"/g' ./Train/annotations/"+ name +".xml"
     os.system(sub)
-    jpg = "mv ./Train/imagensTrain/"+ name +".jpg ./Train/imagensTrain/image"+ str(image).zfill(5) +".jpg"
+    jpg = "mv ./Train/imagensTrain/"+ name +".jpg ./Train/imagensTrain/image"+ str(image).zfill(digits) +".jpg"
     os.system(jpg)
-    xml = "mv ./Train/annotations/"+ name +".xml ./Train/annotations/image"+ str(image).zfill(5) +".xml"
+    xml = "mv ./Train/annotations/"+ name +".xml ./Train/annotations/image"+ str(image).zfill(digits) +".xml"
     os.system(xml)
     image +=1
--- AI/Vision/Train/zipNetwork.sh
+++ AI/Vision/Train/zipNetwork.sh
@@ -1,12 +1,14 @@
 n="$(pwd)"
+rm -R ./models/rede
 mkdir ./models/rede
 
 cd ./models/train
 
 var=$(ls model.ckpt-*.index | sort -V | tail -n 1)
 var=${var%.index}
+echo "Checkpoint "$var
 
-cd ~/models
+cd ~/models/research
 
 python object_detection/export_inference_graph.py \
   --input_type image_tensor \
@@ -19,6 +21,7 @@ mkdir newRede
 cp ./models/rede/frozen_inference_graph.pb ./newRede/frozen_inference_graph.pb
 cp ./data/object-detection.pbtxt ./newRede/object-detection.pbtxt
 cd newRede
+rm ../../Data/newRede.tar.gz
 tar -czf ../../Data/newRede.tar.gz *
 cd ..
 rm -R newRede
